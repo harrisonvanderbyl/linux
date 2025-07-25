e5fef5ee7ccf8c421b7862e5deb4ebeb36ed5e41
Stephan Gerhold <stephan.gerhold@linaro.org>
2025-07-25 15:21:40 +0200

    remoteproc: core: Allow restarting detached remoteprocs with new firmware
    At the moment, the remoteproc core supports only one auto boot "strategy":
    A remoteproc that is already running during boot ("detached") is attached,
    a remoteproc that is offline is started after loading the firmware. This
    works if the firmware loaded during boot is the same that we would start
    later, but it could also be outdated or a reduced size version that is
    missing some functionality. In this case, the best option is to try
    restarting it with new firmware - assuming that it is available while
    booting.
    
    Add support for this alternative behavior by replacing the "auto_boot" bool
    with a more explicit enum rproc_auto_boot.
    
    For RPROC_AUTO_BOOT_RESTART_IF_FW_AVAILABLE, try requesting the firmware
    early and - if successful - perform a clean stop of the remoteproc so that
    it can be restarted with the new firmware afterwards. A remoteproc driver
    making use of this functionality must handle the stop() callback being
    called during the initial detached state.
    
    Signed-off-by: Stephan Gerhold <stephan.gerhold@linaro.org>

--- drivers/remoteproc/xlnx_r5_remoteproc.c
+++ drivers/remoteproc/xlnx_r5_remoteproc.c
@@ -938,7 +938,7 @@ static struct zynqmp_r5_core *zynqmp_r5_add_rproc_core(struct device *cdev)
 
 	rproc_coredump_set_elf_info(r5_rproc, ELFCLASS32, EM_ARM);
 
-	r5_rproc->auto_boot = false;
+	r5_rproc->auto_boot = RPROC_AUTO_BOOT_DISABLED;
 	r5_core = r5_rproc->priv;
 	r5_core->dev = cdev;
 	r5_core->np = dev_of_node(cdev);
